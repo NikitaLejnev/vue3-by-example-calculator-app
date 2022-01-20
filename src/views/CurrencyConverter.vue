f9942dc87b216eefb036f9cdfcaf8716af60890c
NikitaLejnev <catassasin331@gmail.com>
2022-01-20 09:53:17 +0300

    Added page content to CurrencyConverter.vue

---
+++ src/views/CurrencyConverter.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-header translucent>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title> Currency Converter </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content fullscreen>
+      <div id="container">
+        <ion-list>
+          <ion-item>
+            <ion-label :color="!amountValid ? 'danger' : undefined"> Amount to Convert </ion-label>
+            <ion-input
+              class="ion-text-right"
+              type="number"
+              v-model="amount"
+              required
+              placeholder="Amount"
+            ></ion-input>
+          </ion-item>
+        </ion-list>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
