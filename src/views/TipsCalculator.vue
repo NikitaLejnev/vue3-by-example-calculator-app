ff9600cb38848707714a9027d8f1c76aaf36d7b1
NikitaLejnev <catassasin331@gmail.com>
2022-01-24 09:51:42 +0300

    Added <script> in TipsCalculator.vue

--- src/views/TipsCalculator.vue
+++ src/views/TipsCalculator.vue
@@ -57,3 +57,123 @@
     </ion-content>
   </ion-page>
 </template>
+
+<script lang="ts">
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSelect,
+  IonSelectOption,
+  IonInput,
+  IonLabel,
+  IonButton,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
+import { computed, defineComponent, reactive, ref, watch } from "vue";
+import { countries as countriesArray } from "@/constants";
+import { useStore } from "vuex";
+import { TipCalculation } from "@/interfaces";
+import { v4 as uuidv4 } from "uuid";
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  name: "TipsCalculator",
+  components: {
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonSelect,
+    IonSelectOption,
+    IonInput,
+    IonLabel,
+    IonButton,
+    IonList,
+    IonItem,
+  },
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+    const subtotal = ref(0);
+    const countries = reactive(countriesArray);
+    const country = ref("Afghanistan");
+    const tippingRate = computed(() => {
+      if (["United States"].includes(country.value)) {
+        return 20;
+      } else if (
+        ["Canada", "Jordan", "Morocco", "South Africa"].includes(country.value)
+      ) {
+        return 15;
+      } else if (["Germany", "Ireland", "Portugal"].includes(country.value)) {
+        return 5;
+      }
+      return 0;
+    });
+    const amountValid = computed(() => {
+      return +subtotal.value >= 0;
+    });
+
+    const result = reactive({
+      tip: 0,
+      total: 0,
+    });
+
+    const addToHistory = (entry: TipCalculation) => {
+      store.commit("addToHistory", entry);
+    };
+
+    const calculateTip = () => {
+      result.tip = 0;
+      result.total = 0;
+      if (!amountValid.value) {
+        return;
+      }
+      addToHistory({
+        id: uuidv4(),
+        subtotal: subtotal.value,
+        country: country.value,
+      });
+      result.tip = tippingRate.value * 0.01 * subtotal.value;
+      result.total = subtotal.value * (1 + tippingRate.value * 0.01);
+    };
+
+    watch(
+      () => route.query,
+      (query) => {
+        const { id: queryId } = query;
+        const { history } = store.state;
+        const entry = history.find(({ id }: TipCalculation) => id === queryId);
+        if (!entry) {
+          return;
+        }
+        const {
+          subtotal: querySubtotal,
+          country: queryCountry,
+        }: TipCalculation = entry;
+        subtotal.value = querySubtotal;
+        country.value = queryCountry;
+      },
+      { immediate: true }
+    );
+
+    return {
+      subtotal,
+      country,
+      countries,
+      tippingRate,
+      amountValid,
+      result,
+      calculateTip,
+    };
+  },
+});
+</script>
